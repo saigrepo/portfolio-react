b26f19435d3b8c6a47892567b05f8d6c332d5c79
Saiganesh2023 <iasganesh123456@gmail.com>
2024-02-27 19:59:06 +0530

    V-1.2 added course name

--- src/components/bodyComponents/Education.jsx
+++ src/components/bodyComponents/Education.jsx
@@ -1,10 +1,10 @@
 import './Education.css'
 import TempComp from "./TempComp.jsx";
 function Education() {
-    const expObject1 = {jobTitle:"Software Engineering",companyName:"Brila Institute of technology",startDate:"01/2023",
-        endDate:"Present",location:"Chennai, IND"};
+    const expObject1 = {jobTitle:"Brila Institute of technology ",companyName:"M.Tech in Software Engineering",startDate:"01/2023",
+        endDate:"Present",location:"Remote, IND"};
 
-    const expObject2 = {jobTitle:"Electronics and Instrumentation Engineering",companyName:"Amrita Vishwa Vidyapeetham",startDate:"06/2017",
+    const expObject2 = {jobTitle:"Amrita Vishwa Vidyapeetham",companyName:"B.Tech in  Electronics and Instrumentation Engineering",startDate:"06/2017",
         endDate:"06/2021",location:"Chennai, IND"};
     return (
         <div className="education">
